7fbaf8cd8f48340a55cc6196c3b42b025f09d656
Xiuliang Li <xiuliang.li@gi-de.com>
2020-03-12 13:49:19 +0800

    temp

--- tinkertest/src/main/java/com/example/tinkertest/activity/MainActivity.java
+++ tinkertest/src/main/java/com/example/tinkertest/activity/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.tinkertest.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.tinkertest.R;
